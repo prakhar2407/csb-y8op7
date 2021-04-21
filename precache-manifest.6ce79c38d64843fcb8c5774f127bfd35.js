c23b2f907c8a83a17740ec2f51169354fef2f3ec
Code Sandbox <hello@codesandbox.io>
2021-04-21 05:06:44 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.6ce79c38d64843fcb8c5774f127bfd35.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fee754087125f602280da219647853b6",
+    "url": "https://prakhar2407.github.io/csb-y8op7/index.html"
+  },
+  {
+    "revision": "bcf2a5f21878e7a4bb72",
+    "url": "https://prakhar2407.github.io/csb-y8op7/static/js/2.7bd492ba.chunk.js"
+  },
+  {
+    "revision": "34c84a8ba27ea8361f78",
+    "url": "https://prakhar2407.github.io/csb-y8op7/static/js/main.1a1a733a.chunk.js"
+  },
+  {
+    "revision": "a4f831888933f429b8b7",
+    "url": "https://prakhar2407.github.io/csb-y8op7/static/js/runtime-main.302656be.js"
+  }
+]);
